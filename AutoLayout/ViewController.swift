2d240f66dd8022556596ed8e8f346f968e23ef78
Francisco Duran <francisco.duran@koombea.com>
2019-09-10 12:18:48 -0500

    Add files

---
+++ AutoLayout/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  AutoLayout
+//
+//  Created by Mac User on 9/10/19.
+//  Copyright © 2019 Mac User. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ README.md
@@ -0,0 +1,1 @@
+# AutoLayouts-IOS
